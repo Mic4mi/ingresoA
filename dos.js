60e55872b3d36206ab5bafd3ed714cdf4a7a2635
Mic4mi <micahl1981@gmail.com>
2020-08-10 09:41:25 -0300

    ejercicio 2

--- dos.js
+++ dos.js
@@ -5,7 +5,62 @@ marca, precio, peso en kilogramos, tipo(sólido o líquido)
 a)informar el peso total de la compra.
 b)la marca del más caro de los líquidos
 c)la marca del más liviano de los sólidos
-*/function mostrar()
-{
-  alert("dos");
+*/function mostrar() {
+  let continuar = "si";
+  let marca;
+  let precio;
+  let peso;
+  let tipo;
+  let acumPeso = 0;
+  let liquidosCarosPrecio = 0;
+  let liquidosCarosBandera = false;
+  let liquidosCarosMarca;
+  let solidosLivianosPeso = 0;
+  let solidosLivianosPesoBandera = false;
+  let solidosLivianosPesoMarca;
+
+  do {
+    marca = prompt("Ingrese la marca del producto");
+    precio = parseFloat(prompt("Ingrese el precio del producto"));
+    while (precio <= 0 || isNaN(precio)) {
+      precio = parseFloat(prompt("Inválido. Ingrese el precio del producto"));
+    }
+
+    peso = parseFloat(prompt("Ingrese el peso del producto."));
+    while (peso <= 0 || isNaN(peso)) {
+      peso = parseFloat(prompt("Inválido. Ingrese el peso del producto."));
+    }
+
+    tipo = prompt('Ingrese el tipo de producto: "sólido" ó "líquido"').toLowerCase();
+    while (tipo != "sólido" && tipo != "líquido") {
+      tipo = prompt('Inválido. Ingrese el tipo de producto: "sólido" ó "líquido"').toLowerCase();
+    }
+
+    // Peso total
+    acumPeso += peso;
+
+    // El más caro de los líquidos
+    //Más liviano de los sólidos
+    switch (tipo) {
+      case "líquido":
+        if (!(liquidosCarosBandera) || precio > liquidosCarosPrecio) {
+          liquidosCarosPrecio = precio;
+          liquidosCarosBandera = true;
+          liquidosCarosMarca = marca;
+        }
+        break;
+      case "sólido":
+        if (!(solidosLivianosPesoBandera) || peso < solidosLivianosPeso) {
+          solidosLivianosPeso = peso;
+          solidosLivianosPesoBandera = true;
+          solidosLivianosPesoMarca = marca;
+        }
+    }
+
+    continuar = prompt("¿Desea ingresar otro producto?").toLowerCase();
+  } while (continuar === "si");
+
+  console.log("A. El peso total de la compra es: " + acumPeso);
+  console.log("B. El producto líquido más caro es marca " + liquidosCarosMarca + " y cuesta $" + liquidosCarosPrecio);
+  console.log("C. El producto sólido más liviano es marca " + solidosLivianosPesoMarca + " y pesa " + solidosLivianosPeso + "kg.");
 }
--- uno.js
+++ uno.js
@@ -7,7 +7,67 @@ b)la edad promedio en total
 c)la mujer con más temperatura(si la hay)
 pedir datos por prompt y mostrar por document.write o console.log
 */
-function mostrar()
-{
-	alert("uno");
+function mostrar() {
+	let nombre;
+	let temperatura;
+	let sexo;
+	let edad;
+	let contadorMasculino = 0;
+	let contadorFemenino = 0;
+	let acumEdad = 0;
+	let promedioEdad;
+	let mayorTemperaturaMujer = 0;
+	let mayorTemperaturaMujerBandera = false;
+	let mayorTemperaturaMujerNombre;
+
+	for (let i = 0; i < 5; i++) {
+		nombre = prompt("Ingrese nombre");
+		while (!(isNaN(nombre))) {
+			nombre = prompt("Inválido.Ingrese nombre");
+		}
+
+		temperatura = parseFloat(prompt("Ingrese la temperatura de la persona"));
+		while (temperatura <= 32 || temperatura >= 44 || isNaN(temperatura)) {
+			temperatura = parseFloat(prompt("Inválido. Ingrese la temperatura de la persona"));
+		}
+
+		sexo = prompt('Sexo: Ingrese "m" para másculino ó "f" para femenino.').toLowerCase();
+		while (sexo != "m" && sexo != "f") {
+			sexo = prompt('Inválido. Ingrese"m" para másculino ó "f" para femenino.').toLowerCase();
+		}
+
+		edad = parseInt(prompt("Ingrese la edad"));
+		while (edad <= 0 || edad >= 116 || isNaN(edad)) {
+			edad = parseInt(prompt("Inválido. Ingrese la edad"));
+		}
+
+		//promedio Edad
+		acumEdad += edad;
+
+		switch (sexo) {
+			case "f":
+				contadorFemenino++
+				if (!(mayorTemperaturaMujerBandera) || temperatura > mayorTemperaturaMujer) {
+					mayorTemperaturaMujer = temperatura;
+					mayorTemperaturaMujerBandera = true;
+					mayorTemperaturaMujerNombre = nombre;
+				}
+				break;
+			case "m":
+				contadorMasculino++
+				break;
+		}
+	}
+
+	promedioEdad = acumEdad / 5;
+
+	console.log("A. Cantidad de mujeres " + contadorFemenino + ". Cantidad de hombres: " + contadorMasculino + ".");
+	console.log("B. El promedio de todas las edades es de " + promedioEdad + ".");
+
+	if (contadorFemenino === 0) {
+		console.log("C. No hay mujeres.");
+	} else {
+		console.log("C. La mujer con mayor temperatura tiene " + mayorTemperaturaMujer + "° y se llama " + mayorTemperaturaMujerNombre);
+	}
+
 }
